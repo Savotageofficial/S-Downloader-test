c99adf268885aebb6159d46e560a61f187d0fb2b
palmhillstry <mohamedgamesar4321@gmail.com>
2026-03-02 01:45:26 +0200

    added a client tracking code to prevent malicious access during development

--- downloader/views.py
+++ downloader/views.py
@@ -8,6 +8,17 @@ from pytubefix.exceptions import VideoUnavailable, RegexMatchError
 
 # Create your views here.
 
+def get_client_ip(request):
+    """
+    Returns the IP of the request, accounting for the possibility of being behind a proxy.
+    """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # The first IP in the list is typically the original client IP.
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def home(request):
 
@@ -15,7 +26,7 @@ def home(request):
 
 
     # return FileResponse(file , as_attachment=True, filename="me-as-an-emperor.png")
-    print("someone is opening the link")
+    print(f"someone is opening the link from{get_client_ip(request)}")
     return render(request, "home.html")
 
 
