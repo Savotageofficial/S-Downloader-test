f2a54b678d6d6b6c09f2917896d080243b8c4602
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-13 01:21:49 +0200

    updated the sitemap

--- downloader/sitemaps.py
+++ downloader/sitemaps.py
@@ -2,7 +2,7 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.5
+    priority = 1
     changefreq = 'daily'
 
     def items(self):
@@ -10,3 +10,17 @@ class StaticViewSitemap(Sitemap):
 
     def location(self, item):
         return reverse(item)
+
+    def get_urls(self, page=1, site=None, protocol=None):
+        protocol = 'https'
+        domain = 'sdownloader.duckdns.org'
+        urls = []
+        for item in self.items():
+            urls.append({
+                'item': item,
+                'location': f'{protocol}://{domain}{self.location(item)}',
+                'lastmod': None,
+                'changefreq': self.changefreq,
+                'priority': self.priority,
+            })
+        return urls
